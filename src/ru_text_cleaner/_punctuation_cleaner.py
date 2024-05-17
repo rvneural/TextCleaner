5998acb81ea56392a24100409be232ce561d4532
Artem Gafarov <a.m.gafarov@ya.ru>
2024-05-17 18:03:22 +0300

    Delete empty unicode symbols

--- src/ru_text_cleaner/_punctuation_cleaner.py
+++ src/ru_text_cleaner/_punctuation_cleaner.py
@@ -7,5 +7,6 @@ def clean_punctuation(self, text: str) -> str:
 
     return ''.join(i for i in text if i not in string.punctuation) - возвращает текст без пунктуационных знаков 
     """
+    text = text.replace('-', ' ')
     return ''.join(i for i in text if i not in string.punctuation)
 
