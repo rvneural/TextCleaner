d04ef1a1431b5a9a359b92c918de8f7b89592f14
Artem Gafarov <a.m.gafarov@ya.ru>
2024-05-17 15:54:17 +0300

    Upload settings

--- src/ru_text_cleaner/__init__.py
+++ src/ru_text_cleaner/__init__.py
@@ -1,9 +1,9 @@
-import _to_lower as to_lower_text
-import _emoji_cleaner as clean_emoji
-import _spaces_cleaner as clean_spaces
-import _html_cleaner as clean_html
-import _punctuation_cleaner as clean_punctuation
-import _clean_stopwords as clean_stopwords
-import _to_morpheme as text_to_morphems
+from ._to_lower import to_lower_text
+from ._emoji_cleaner import clean_emoji
+from ._spaces_cleaner import clean_spaces
+from ._html_cleaner import clean_html
+from ._punctuation_cleaner import clean_punctuation
+from ._clean_stopwords import clean_stopwords
+from ._to_morpheme import text_to_morphems
 
-import TextCleaner as TextCleaner
+from .TextCleaner import TextCleaner
--- src/ru_text_cleaner/_emoji_cleaner.py
+++ src/ru_text_cleaner/_emoji_cleaner.py
@@ -1,33 +1,10 @@
 import re
+import emoji
 
 
 def clean_emoji(self, text: str) -> str:
-
     """
         Функция clean_emoji удаляет все emoji из текста
+    """
 
-        moji_pattern = re.compile("["
-                                   u"\U0001F600-\U0001F64F"
-                                   u"\U0001F300-\U0001F5FF"
-                                   u"\U0001F680-\U0001F6FF"
-                                   u"\U0001F1E0-\U0001F1FF"
-                                   "]+", flags=re.UNICODE)
-        Коомпилирует шаблон эмодзи
-
-        return emoji_pattern.sub(r'', text) - Возвращает текст без emoji
-
-        Параметры
-        ------
-            "[" u"\U0001F600-\U0001F64F" u"\U0001F300-\U0001F5FF" u"\U0001F680-\U0001F6FF" u"\U0001F1E0-\U0001F1FF" "]+" - шаблон с эмодзи
-            flags=re.UNICODE - флаг кодировки
-            r'' - заменяет emoji на пустоту
-        """
-
-    emoji_pattern = re.compile("["
-                               u"\U0001F600-\U0001F64F"
-                               u"\U0001F300-\U0001F5FF"
-                               u"\U0001F680-\U0001F6FF"
-                               u"\U0001F1E0-\U0001F1FF"
-                               "]+", flags=re.UNICODE)
-    return emoji_pattern.sub(r'', text)
-
+    return ''.join(t for t in text if not emoji.is_emoji(t))
