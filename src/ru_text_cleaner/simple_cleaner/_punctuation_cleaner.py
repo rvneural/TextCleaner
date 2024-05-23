9f764f734508c405fc9c9eb7bc8fbbffe46f1b4b
Artem Gafarov <a.m.gafarov@ya.ru>
2024-05-23 20:53:10 +0300

    Adapted for tensforflow strings

--- src/ru_text_cleaner/simple_cleaner/_punctuation_cleaner.py
+++ src/ru_text_cleaner/simple_cleaner/_punctuation_cleaner.py
@@ -10,7 +10,7 @@ def clean_punctuation(text: str) -> str:
     text = text.replace('-', ' ')
     text = text.replace('–', ' ')
     text = text.replace('—', ' ')
-    text.replace('«', ' ')
-    text.replace('»', ' ')
+    text = text.replace('«', ' ')
+    text = text.replace('»', ' ')
     return ''.join(i for i in text if i not in string.punctuation)
 
